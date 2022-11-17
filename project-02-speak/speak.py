529c7162f972d00792676991dfee239984b4b08b
Achapko200 <111392651+Achapko200@users.noreply.github.com>
2022-11-17 15:20:34 -0500

    Add files via upload

---
+++ project-02-speak/speak.py
@@ -0,0 +1,18 @@
+with open('silly.txt') as f:
+    lines = f.read()
+
+Mydict = {"sunk ship":"scuttle'",
+          "wooden leg":"peg leg",
+          "cannon ready to fire":"fire in the hole",
+          "friends and comrades": "hearties"}
+
+
+for i in Mydict:
+    #print(Mydict[i])
+    lines = lines.replace(i,Mydict[i])
+    lines = lines.replace(i.upper(),Mydict[i])
+    lines = lines.replace(i.lower(),Mydict[i])
+    lines = lines.replace(i.title(),Mydict[i])
+    lines = lines.replace(i.capitalize(),Mydict[i])
+    
+print(lines)
